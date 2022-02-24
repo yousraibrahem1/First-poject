ac38f3149b6339e565df47bceb1949ebe6f0ce0b
yousraibrahem1 <yousra.ibrahem@gccpay.sa>
2022-02-24 14:55:49 +0300

    =first commit

--- src/componnats/ExpenseItem.js
+++ src/componnats/ExpenseItem.js
@@ -1,19 +1,15 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
-function ExpenseItem() {
-  const ExpesnseDate = new Date(2021, 2, 28);
-  const expesnseTitle = "car insurance";
-  const expesnseAmount = 29300.11;
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div> {ExpesnseDate.toISOString()}</div>
+    <ExpenseDate date= {props.date}  />
       <div className="expense-item__description">
-        <h2>{expesnseTitle}</h2>
-        <div className="expense-item__price"> ${expesnseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price"> ${props.amount}</div>
       </div>
     </div>
   );
 }
-
 export default ExpenseItem;
